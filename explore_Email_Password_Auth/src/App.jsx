37fadd294bf7eb0f81d0c2a9e3bf416250cfc9c8
MdShajalalsojib <shahjalalsojib088@gmail.com>
2026-07-28 22:00:44 +0600

    first commit

---
+++ explore_Email_Password_Auth/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+   
+  return (
+    
+      <>
+      <h1 className='text-blue-400 font-bold text-center'>shajalal </h1>
+      <p className='text-center font-bold text-cyan-900'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam, commodi?</p>
+      </>
+    
+  )
+}
+
+export default App
